cfad113464d5bc02c5c22237da0b34a0a1f1e1db
himanshu bhardwaj <him_bhar@hotmail.com>
2013-03-21 13:40:14 +0530

    Improved unit test cases.

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 This is a project for a simple aspectj in java without any other supporting frameworks. like spring
 
 To have support for aspectj in eclipse, you will need to download the AJDT for that particular eclipse version from eclipse marketplace.
+
+When testing with main or test cases, Here it will be just the response from main method, but when running goal mvn test or mvn install the aspect will run
---
+++ src/test/java/com/himanshu/sample/main/test/HelloWorldTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 Himanshu Bhardwaj
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+package com.himanshu.sample.main.test;
+
+import org.junit.Test;
+
+import com.himanshu.sample.main.HelloWorld;
+
+public class HelloWorldTest {
+
+	//Here it will be just the response from main method, but when running goal mvn test or mvn install the aspect will run
+	@Test
+	public void testMain() {
+		HelloWorld.main(new String[]{});
+
+	}
+
+}
